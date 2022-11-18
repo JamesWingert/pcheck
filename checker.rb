9b893a6fcc09cff97599796e7de3e34c1452fb5e
James Wingert <james.wingert@strongmind.com>
2022-11-17 17:00:35 -0700

    Added user response for iteration 3

--- checker.rb
+++ checker.rb
@@ -1,15 +1,23 @@
 class Checker
   def check(password)
-    if password.length < 7 || !password.match(/[a-zA-Z]/) || !password.match(/[0-9]/)
-      return false
+    if password.length < 7
+      puts "Password must be at least 7 characters long"
+      false
+    elsif !password.match(/[a-zA-Z]/)
+      puts "Password must contain at least one letter"
+      false
+    elsif !password.match(/[0-9]/)
+      puts "Password must contain at least one number"
+      false
     end
-    return true
   end
+
+  true
 
   def admin_check(password)
     if password.length < 10 || !password.match(/\W/)
       return false
     end
-    return check(password)
+    check(password)
   end
 end
